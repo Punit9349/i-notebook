33e498ffb67e988cda3edeb91e7c14bb17a2c960
Punit9349 <puneetgarg9349@gmail.com>
2022-06-14 23:41:31 +0530

    video 47 comp

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -3,27 +3,42 @@ const User= require('../models/User');
 const router= express.Router();
 const { body, validationResult } = require('express-validator');
 
-// create a User using: POST "/api/auth". Doesn't require Auth.  
-router.post('/',[ 
+// create a User using: POST "/api/auth/createuser". No login required.  
+router.post('/createuser',[ 
      body('name','Enter a valid name').isLength({ min: 3 }),
      body('email', 'Enter a valid email').isEmail(),
      body('password','Password must be of atleast 5 characters').isLength({ min: 5 }),
     
-    ], (req,res)=>{  
+    ], async (req,res)=>{ 
+        // If there are errors, return Bad requests and errors. 
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
           return res.status(400).json({ errors: errors.array() });
         }
 
-        User.create({
+        // check whether user with this email exists already
+        try{
+        let user= await User.findOne({email: req.body.email});
+        console.log(user);
+        if(user){
+            return res.status(400).json({error: "Sorry a user with this email already exists"})
+        }
+        // create a new user
+         user= await User.create({
             name: req.body.name,
             password: req.body.password,
             email: req.body.email,
-          }).then(user => res.json(user))
-          .catch(err=>{console.log(err)
-            res.json({error:'Please enter a valid email address', message:err.message})
-        
-        })
-})
+          })
+          
+        //   .then(user => res.json(user))
+        //   .catch(err=>{console.log(err)
+        //     res.json({error:'Please enter a valid email address', message:err.message}) })
+
+        res.json({user});
+        } catch(error){
+            console.error(error.message);
+            res.status(500).send("Some error occured");
+        }
+    })
 
 module.exports=router
