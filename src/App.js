886e2ecd3d852b7420329806af20af5a90270356
Punit9349 <puneetgarg9349@gmail.com>
2022-06-21 00:53:18 +0530

    Adding useContext hook in our app

--- src/App.js
+++ src/App.js
@@ -4,21 +4,20 @@ import Home from "./Components/Home";
 import About from "./Components/About";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NoteState from "./context/notes/NoteState";
 
 function App() {
   return (
     <>
-      <Router>
-        <Navbar />
-
-        <Routes> 
-        <Route path="/" element={<Home/>} />
-
-        <Route path="/about" element={<About/>} />
-
-
-        </Routes>
-      </Router>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <Routes> 
+            <Route path="/" element={<Home/>} />
+            <Route path="/about" element={<About/>} />
+          </Routes>
+        </Router>
+      </NoteState>
     </>
   );
 }
